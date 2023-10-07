7501893dc2c4cfcd933430093accb83d047eba50
Cr3ativee <103892133+Cr3ativee@users.noreply.github.com>
2023-10-07 11:18:29 -0500

    Delete app directory

--- app/script.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the button element
-const TunnelRush = document.getElementById("redirectButton");
-
-// Add a click event listener to the button
-TunnelRush.addEventListener("click", function () {
-  // Redirect to the specified URL when the button is clicked
-  window.location.href = "https://smartfoloolol.pages.dev/tunnel-rush"; // Replace with your desired URL
-});
